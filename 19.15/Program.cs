bf4d2c1cce20b0a81ed580f70a6ac183e0c11c2e
giupe <giupe@MSI>
2025-09-20 17:00:19 +0200

    Add project files.

---
+++ 19.15/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _19._15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numeri = new List<int>();
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                numeri.Add(rnd.Next(1, 11));
+            }
+
+            foreach (int n in numeri)
+                Console.WriteLine(n);
+
+            int sommapari = 0;
+            int sommadispari = 0;
+            foreach (int n in numeri)
+            {
+                if (n % 2 == 0)
+                    sommapari += n;
+                else
+                    sommadispari += n;
+            }
+            Console.WriteLine($"La somma dei numeri pari è {sommapari}");
+            Console.WriteLine($"La somma dei numeri dispari è {sommadispari}");
+        }
+    }
+}
